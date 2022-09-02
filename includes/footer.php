cf42629ed1da1fcf2b2261640220f084ae370da7
Script_Kiddie <92115131+erdifirnanto@users.noreply.github.com>
2022-09-02 11:18:43 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+         <!-- Footer -->
+         <footer class="sticky-footer bg-white">
+             <div class="container my-auto">
+                 <div class="copyright text-center my-auto fixed-bottom">
+                     <span>Copyright &copy; Erdi Vernanto</span>
+                 </div>
+             </div>
+         </footer>
+         <!-- End of Footer -->
+       
